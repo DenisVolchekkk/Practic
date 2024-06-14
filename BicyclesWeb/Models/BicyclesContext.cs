dd312b443ad0ae64a47a6e4f183b88a6614b7f6f
Денис <denisvochek@mail.ru>
2024-06-14 19:59:25 +0300

    com

--- BicyclesWeb/Models/BicyclesContext.cs
+++ BicyclesWeb/Models/BicyclesContext.cs
@@ -11,12 +11,13 @@ namespace BicyclesWeb.Models
     : base(options)
         {
         }
-        public virtual DbSet<Bicycle> Bicycles { get; set; }
-        public virtual DbSet<Part> Parts { get; set; } 
-        public virtual DbSet<PartBicycle> PartBicycles { get; set; }
-        public virtual DbSet<PartOrder> PartOrders { get; set; }
-        public virtual DbSet<Supplier> Suppliers { get; set; } 
         public virtual DbSet<SupplierType> SupplierTypes { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Part> Parts { get; set; }
+        public virtual DbSet<Bicycle> Bicycles { get; set; }
+
+        public virtual DbSet<PartOrder> PartOrders { get; set; }
+        public virtual DbSet<PartBicycle> PartBicycles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Bicycles;Integrated Security=SSPI;Trusted_Connection=True;TrustServerCertificate=True");
